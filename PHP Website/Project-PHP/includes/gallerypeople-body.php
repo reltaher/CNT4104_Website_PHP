37c127211180651cef24d3591e15eaf23289a7fa
krislowg <46728217+krislowg@users.noreply.github.com>
2020-12-04 15:55:49 -0500

    Gallery people updated

--- PHP Website/Project-PHP/includes/gallerypeople-body.php
+++ PHP Website/Project-PHP/includes/gallerypeople-body.php
@@ -2,9 +2,136 @@
 
 									<div class="text-center">
 
-									<h1 class="jumbotron-heading">Gallery: People</h1>
-									<hr>
+										<h1 class="jumbotron-heading">Gallery: People</h1>
+										<hr>
 
 									</div>
-									
 								</div>
+								<div class="album py-5 bg-light">
+									<div class="container">
+										<div class="row">
+
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow">
+													<img class="card-img-top" src="images/people/ZalAllende.JPG" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">With Isabel Allende.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow">
+													<img class="card-img-top" src="images/people/ZalWithAmram1.jpg" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">With David Amram.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow">
+													<img class="card-img-top" src="images/people/ZalWithBillMorgan.jpg" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">With Bill Morgan.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow">
+													<img class="card-img-top" src="images/people/ZalWithCherkovski.jpg" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">With Neely Cherkovski.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow">
+													<img class="card-img-top" src="images/people/ZalWithChubbyChecker.JPG" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">With Chubby Checker.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow">
+													<img class="card-img-top" src="images/people/ZalWithFerlinghetti4.jpg" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">With Lawrence Ferlinghetti.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow">
+													<img class="card-img-top" src="images/people/ZalWithGiorno2.JPG" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">With John Giorno.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow">
+													<img class="card-img-top" src="images/people/ZalWithGoode.jpg" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">With Joe Goode.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow">
+													<img class="card-img-top" src="images/people/ZalWithHass2.JPG" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">With Robert Hass.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow">
+													<img class="card-img-top" src="images/people/ZalWithHirschman.jpg" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">With Jack Hirshman.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow">
+													<img class="card-img-top" src="images/people/ZalWithJong.jpg" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">With Erica Jong.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow">
+													<img class="card-img-top" src="images/people/ZalWithMarius2.JPG" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">With Marius Kociejowski.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow">
+													<img class="card-img-top" src="images/people/ZalWithMcClure.jpg" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">With Michael McClure.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow">
+													<img class="card-img-top" src="images/people/ZalWithMeltzer2.jpg" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">With David Meltzer.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow">
+													<img class="card-img-top" src="images/people/ZalWithWinans.jpg" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">With A.D. Winans</p>
+													</div>
+												</div>
+											</div>											
+										</div>
+									</div>
+								</div>
+							</div>
