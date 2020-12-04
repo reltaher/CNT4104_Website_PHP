02151f1d2a4ba3a6829d93e2e9a2534e167ca3c3
reltaher <43248170+reltaher@users.noreply.github.com>
2020-12-04 16:53:25 -0500

    Add files via upload

--- PHP Website/Project-PHP/includes/bookreviews-body.php
+++ PHP Website/Project-PHP/includes/bookreviews-body.php
@@ -1,25 +1,53 @@
-<div id="about" class="section">
+								<div class="container">
 
-		<div class="container">
+									<div class="text-center">
 
-				<h4 class="aboutHeading">BOOK REVIEWS</h4>
-				<hr>
+										<h1 class="jumbotron-heading">Book Reviews</h1>
+										<hr>
 
-				<h5><a href="http://www.computingreviews.com/review/review_review.cfm?review_id=144178">Cyber-physical attacks: a growing invisible threat</a></h5>
-				
-				<p>by G.Loukas Butterworth-Heinemann, Newton, Mass., 2015</p>
-				<hr>
-				
-				<h5><a href="http://www.computingreviews.com/review/review_review.cfm?review_id=141990">The practice of network security monitoring: understanding incident detection and response</a></h5>
-				
-				<p>by R. Bejtlich No Starch Press, San Francisco, Calif., 2013</p>
-				<hr>
-				
-				<h5><a href="https://www.amazon.com/review/R1X8RPEXL7JUY7">The Tangled Web: A Guide to Securing Modern Web Applications</a></h5>
- 
-        <p>by Michael Zalewski no starch press, San Francisco, 2011</p>
-        
+									</div>
 
-				</div>
+								</div>
 
-		</div>
+								<div class="container">
+
+									<div class="row">
+
+										<div class="col text-center">
+
+											<h5><a href="http://www.computingreviews.com/review/review_review.cfm?review_id=144178">Cyber-physical attacks: a growing invisible threat</a></h5>
+											
+											<p>by G.Loukas Butterworth-Heinemann, Newton, Mass., 2015</p>
+											<hr>
+
+										</div>
+									</div>
+
+									<div class="row">
+
+										<div class="col text-center">
+											
+											<h5><a href="http://www.computingreviews.com/review/review_review.cfm?review_id=141990">The practice of network security monitoring: understanding incident detection and response</a></h5>
+											
+											<p>by R. Bejtlich No Starch Press, San Francisco, Calif., 2013</p>
+											<hr>
+
+										</div>
+									</div>
+
+									<div class="row">
+
+										<div class="col text-center">
+											
+											<h5><a href="https://www.amazon.com/review/R1X8RPEXL7JUY7">The Tangled Web: A Guide to Securing Modern Web Applications</a></h5>
+											
+											<p>by Michael Zalewski no starch press, San Francisco, 2011</p>
+
+										</div>
+
+									</div>
+									
+
+								</div>
+
+							</div>
