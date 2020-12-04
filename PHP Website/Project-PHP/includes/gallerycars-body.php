997da0579be71e820fc4e764ffc5ddf911ef4293
krislowg <46728217+krislowg@users.noreply.github.com>
2020-12-04 16:56:25 -0500

    Gallery Cars updated

--- PHP Website/Project-PHP/includes/gallerycars-body.php
+++ PHP Website/Project-PHP/includes/gallerycars-body.php
@@ -1,32 +1,65 @@
-								<div class="container">
+							 	<div class="container">
 
 									<div class="text-center">
 
-									<h1 class="jumbotron-heading">Gallery: Cars</h1>
-									<hr>
+										<h1 class="jumbotron-heading">Gallery: Cars</h1>
+										<hr>
 
 									</div>
+								</div>
+								<div class="album py-5 bg-light">
+									<div class="container">
+										<div class="row">
 
-									<div class="row">
-										<div id="lightgallery">
-											<a class="gallery" href="images/ZalCars1-Mercedes.jpg" data-sub-html="Car 1">
-												<img class="img-fluid img-thumbnail" src="images/ZalCars1-Mercedes.jpg" />
-											</a>
-											<a class="gallery" href="images/ZalCars2-Warszawa.jpg" data-sub-html="Car 2">
-												<img class="img-fluid img-thumbnail" src="images/ZalCars2-Warszawa.jpg" />
-											</a>
-											<a class="gallery" href="images/ZalCars3-Mustang.jpg" data-sub-html="Car 3">
-												<img class="img-fluid img-thumbnail" src="images/ZalCars3-Mustang.jpg" />
-											</a>
-											<a class="gallery" href="images/ZalCars4-Ranger.jpg" data-sub-html="Car 4">
-												<img class="img-fluid img-thumbnail" src="images/ZalCars4-Ranger.jpg" />
-											</a>
-											<a class="gallery" href="images/ZalCars5-BeatMuseum.jpg" data-sub-html="Car 5">
-												<img class="img-fluid img-thumbnail" src="images/ZalCars5-BeatMuseum.jpg" />
-											</a>
-											<a class="gallery" href="images/ZalCars6-Gold.jpg" data-sub-html="Car 6">
-												<img class="img-fluid img-thumbnail" src="images/ZalCars6-Gold.jpg" />
-											</a>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow gallery">
+													<img class="card-img-top" src="images/ZalCars1-Mercedes.JPG" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">Some example text to build on the image title and make up the bulk.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow gallery">
+													<img class="card-img-top" src="images/ZalCars2-Warszawa.jpg" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">Some example text to build on the image title and make up the bulk.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow gallery">
+													<img class="card-img-top" src="images/ZalCars3-Mustang.jpg" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">Some example text to build on the image title and make up the bulk.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow gallery">
+													<img class="card-img-top" src="images/ZalCars4-Ranger.JPG" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">Some example text to build on the image title and make up the bulk.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow gallery">
+													<img class="card-img-top" src="images/ZalCars5-BeatMuseum.jpg" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">Some example text to build on the image title and make up the bulk.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow gallery">
+													<img class="card-img-top" src="images/ZalCars6-Gold.JPG" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">Some example text to build on the image title and make up the bulk.</p>
+													</div>
+												</div>
+											</div>															
 										</div>
 									</div>
 								</div>
+		         				 </div>
