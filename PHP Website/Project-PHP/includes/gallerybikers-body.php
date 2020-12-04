b4ad43cafb2e66b1097b76bb65a9d52c26ca7b07
reltaher <43248170+reltaher@users.noreply.github.com>
2020-12-04 13:36:31 -0500

    Add files via upload

---
+++ PHP Website/Project-PHP/includes/gallerybikers-body.php
@@ -0,0 +1,69 @@
+								<div class="container">
+
+									<div class="text-center">
+
+									<h1 class="jumbotron-heading">Gallery: Bikers</h1>
+									<hr>
+
+									</div>
+
+									<div class="row">
+										<div id="lightgallery">
+											<a class="gallery" href="images/bikers/DobroslawaDaytona-Bike4.jpg" data-sub-html="Bike 1">
+												<img class="img-fluid img-thumbnail" src="images/bikers/DobroslawaDaytona-Bike4.jpg" />
+											</a>
+											<a class="gallery" href="images/bikers/DobroslawaDaytona-Bike5.jpg" data-sub-html="Bike 2">
+												<img class="img-fluid img-thumbnail" src="images/bikers/DobroslawaDaytona-Bike5.jpg" />
+											</a>
+											<a class="gallery" href="images/bikers/DobroslawaDaytona-Car.jpg" data-sub-html="Bike 3">
+												<img class="img-fluid img-thumbnail" src="images/bikers/DobroslawaDaytona-Car.jpg" />
+											</a>
+											<a class="gallery" href="images/bikers/DobroslawaDaytona-DirtyHarry.jpg" data-sub-html="Bike 1">
+												<img class="img-fluid img-thumbnail" src="images/bikers/DobroslawaDaytona-DirtyHarry.jpg" />
+											</a>
+											<a class="gallery" href="images/bikers/DominikaDallas-RoyOrbison.jpg" data-sub-html="Bike 2">
+												<img class="img-fluid img-thumbnail" src="images/bikers/DominikaDallas-RoyOrbison.jpg" />
+											</a>
+											<a class="gallery" href="images/bikers/DominikaDaytona-Bike1.jpg" data-sub-html="Bike 3">
+												<img class="img-fluid img-thumbnail" src="images/bikers/DominikaDaytona-Bike1.jpg" />
+											</a>
+											<a class="gallery" href="images/bikers/DominikaDaytona-Harley.jpg" data-sub-html="Bike 1">
+												<img class="img-fluid img-thumbnail" src="images/bikers/DominikaDaytona-Harley.jpg" />
+											</a>
+											<a class="gallery" href="images/bikers/DominikaDaytona-Harley1.jpg" data-sub-html="Bike 2">
+												<img class="img-fluid img-thumbnail" src="images/bikers/DominikaDaytona-Harley1.jpg" />
+											</a>
+											<a class="gallery" href="images/bikers/ride1.jpg" data-sub-html="Bike 3">
+												<img class="img-fluid img-thumbnail" src="images/bikers/ride1.jpg" />
+											</a>
+											<a class="gallery" href="images/bikers/ride2.jpg" data-sub-html="Bike 1">
+												<img class="img-fluid img-thumbnail" src="images/bikers/ride2.jpg" />
+											</a>
+											<a class="gallery" href="images/bikers/ride3.jpg" data-sub-html="Bike 2">
+												<img class="img-fluid img-thumbnail" src="images/bikers/ride3.jpg" />
+											</a>
+											<a class="gallery" href="images/bikers/ride4.jpg" data-sub-html="Bike 3">
+												<img class="img-fluid img-thumbnail" src="images/bikers/ride4.jpg" />
+											</a>
+											<a class="gallery" href="images/bikers/ride5.jpg" data-sub-html="Bike 4">
+												<img class="img-fluid img-thumbnail" src="images/bikers/ride5.jpg" />
+											</a>
+											<a class="gallery" href="images/bikers/Zalewski-BikeDaytona0.jpg" data-sub-html="Bike 3">
+												<img class="img-fluid img-thumbnail" src="images/bikers/Zalewski-BikeDaytona0.jpg" />
+											</a>
+											<a class="gallery" href="images/bikers/Zalewski-BikeDaytona1.jpg" data-sub-html="Bike 1">
+												<img class="img-fluid img-thumbnail" src="images/bikers/Zalewski-BikeDaytona1.jpg" />
+											</a>
+											<a class="gallery" href="images/bikers/Zalewski-BikeDaytona2.jpg" data-sub-html="Bike 2">
+												<img class="img-fluid img-thumbnail" src="images/bikers/Zalewski-BikeDaytona2.jpg" />
+											</a>
+											<a class="gallery" href="images/bikers/Zalewski-BikeRomeNY.jpg" data-sub-html="Bike 3">
+												<img class="img-fluid img-thumbnail" src="images/bikers/Zalewski-BikeRomeNY.jpg" />
+											</a>
+											<a class="gallery" href="images/bikers/ZalewskiHarley-Milwaukee.jpg" data-sub-html="Bike 4">
+												<img class="img-fluid img-thumbnail" src="images/bikers/ZalewskiHarley-Milwaukee.jpg" />
+											</a>
+										</div>
+									</div>
+
+								</div>
