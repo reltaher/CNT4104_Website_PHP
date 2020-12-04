65e9c80f331203671e1cd639a7f43664f5d4e3c8
reltaher <43248170+reltaher@users.noreply.github.com>
2020-12-04 16:41:48 -0500

    Add files via upload

--- PHP Website/Project-PHP/includes/beatGeneration-body.php
+++ PHP Website/Project-PHP/includes/beatGeneration-body.php
@@ -1,20 +1,52 @@
-<div id="about" class="section">
+								<div class="container">
 
-		<div class="container">
+									<div class="text-center">
 
-				<h4 class="aboutHeading">THE BEAT GENERATION REVIEWS</h4>
-				<hr>
+										<h1 class="jumbotron-heading">The Beat Generation</h1>
+										<hr>
 
-				<h5><a href="#">Contex 1</a></h5>
-				
-				<p>Text .... </p>
-				<hr>
-				
-				<h5><a href="#">Contex 2</a></h5>
-				
-				<p>Text .... </p>
-				<hr>
-                
-				</div>
+									</div>
 
-		</div>
+								</div>
+
+									<div class="container">
+
+										<div class="row">
+
+											<div class="col text-center">
+
+												<h5><a href="http://www.inter-zone.org/adwinans3.html">Photo at The Beat Museum, San Francisco</a></h5>
+												<p>A.D. Winans, 2011</p>
+												<hr>
+
+											</div>
+
+										</div>
+
+										<div class="row">
+
+											<div class="col text-center">
+
+												<h5><a href="http://writingpolishdiaspora.blogspot.com/2010/07/ferlinghetti-in-polish.html">Caffè Trieste, San Francisco</a></h5>
+												<p>Lawrence Ferlinghetti, 2010</p>
+												<hr>
+
+											</div>
+
+										</div>
+
+										<div class="row">
+
+											<div class="col text-center">
+
+												<h5><a href="https://en.wikipedia.org/wiki/Jack_Hirschman">At the City Lights Bookstore, San Francisco</a></h5>
+												<p>Jack Hirschman, 2007</p>
+												<hr>
+
+											</div>
+
+										</div>
+
+									</div>
+
+								</div>
--- PHP Website/Project-PHP/includes/writers-body.php
+++ PHP Website/Project-PHP/includes/writers-body.php
@@ -1,34 +1,49 @@
-<div id="about" class="section">
+								<div class="container">
 
-		<div class="container">
+									<div class="text-center">
 
-				<h4 class="aboutHeading">POLISH AMERCIAN WRITERS REVIEWS</h4>
-				<hr>
+										<h1 class="jumbotron-heading">Polish-American Writers</h1>
+										<hr>
 
-				<h5><a href="http://writingpolishdiaspora.blogspot.com/2013/02/douglas-blazek-at-moes-in-berkeley.html">Reading in Berkeley and Moe’s Bookstore</a></h5>
-				<p>Douglas Blazek, 2013</p>
-				<hr>
-				
-				<h5><a href="http://www.inter-zone.org/adwinans3.html">Photo at The Beat Museum, San Francisco</a></h5>
-				<p>A.D. Winans, 2011</p>
-				<hr>
-                
-                <h5><a href="http://www.stephenpoleskie.com/works.htm">Photo at the Rauschenberg Painting</a></h5>
-				<p>Steve Poleskie, 2011</p>
-                <hr>
-                
-                <h5><a href="http://writingpolishdiaspora.blogspot.com/2010/07/ferlinghetti-in-polish.html">Caffè Trieste, San Francisco</a></h5>
-				<p>Lawrence Ferlinghetti, 2010</p>
-                <hr>
+									</div>
 
-                <h5><a href="https://en.wikipedia.org/wiki/Jack_Hirschman">At the City Lights Bookstore, San Francisco</a></h5>
-				<p>Jack Hirschman, 2007</p>
-                <hr>
+									<div class="container">
 
-                <h5><a href="https://www.jstor.org/stable/20148780?seq=1">Magical Polonism</a></h5>	
-				<p>article in Polish American Studies, 2007</p>
-                <hr>
+										<div class="row">
 
-				</div>
+											<div class="col text-center">
 
-		</div>
+												<h5><a href="http://writingpolishdiaspora.blogspot.com/2013/02/douglas-blazek-at-moes-in-berkeley.html">Reading in Berkeley and Moe’s Bookstore</a></h5>
+												<p>Douglas Blazek, 2013</p>
+												<hr>
+
+											</div>
+										</div>
+
+										<div class="row">
+											<div class="col text-center">
+
+												<h5><a href="http://www.stephenpoleskie.com/works.htm">Photo at the Rauschenberg Painting</a></h5>
+												<p>Steve Poleskie, 2011</p>
+												<hr>
+
+											</div>
+										</div>
+
+										<div class="row">
+											<div class="col text-center">
+
+
+												<h5><a href="https://www.jstor.org/stable/20148780?seq=1">Magical Polonism</a></h5>	
+												<p>Article in Polish American Studies, 2007</p>
+												<hr>
+
+											</div>
+
+										</div>
+
+									</div>
+
+								</div>
+
+							</div>
