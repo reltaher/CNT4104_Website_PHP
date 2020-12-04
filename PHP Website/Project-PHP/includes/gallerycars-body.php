c76fc9479f2365417e4fd0141bf98b3a7e301938
reltaher <43248170+reltaher@users.noreply.github.com>
2020-12-04 13:37:20 -0500

    Add files via upload

---
+++ PHP Website/Project-PHP/includes/gallerycars-body.php
@@ -0,0 +1,32 @@
+								<div class="container">
+
+									<div class="text-center">
+
+									<h1 class="jumbotron-heading">Gallery: Cars</h1>
+									<hr>
+
+									</div>
+
+									<div class="row">
+										<div id="lightgallery">
+											<a class="gallery" href="images/ZalCars1-Mercedes.jpg" data-sub-html="Car 1">
+												<img class="img-fluid img-thumbnail" src="images/ZalCars1-Mercedes.jpg" />
+											</a>
+											<a class="gallery" href="images/ZalCars2-Warszawa.jpg" data-sub-html="Car 2">
+												<img class="img-fluid img-thumbnail" src="images/ZalCars2-Warszawa.jpg" />
+											</a>
+											<a class="gallery" href="images/ZalCars3-Mustang.jpg" data-sub-html="Car 3">
+												<img class="img-fluid img-thumbnail" src="images/ZalCars3-Mustang.jpg" />
+											</a>
+											<a class="gallery" href="images/ZalCars4-Ranger.jpg" data-sub-html="Car 4">
+												<img class="img-fluid img-thumbnail" src="images/ZalCars4-Ranger.jpg" />
+											</a>
+											<a class="gallery" href="images/ZalCars5-BeatMuseum.jpg" data-sub-html="Car 5">
+												<img class="img-fluid img-thumbnail" src="images/ZalCars5-BeatMuseum.jpg" />
+											</a>
+											<a class="gallery" href="images/ZalCars6-Gold.jpg" data-sub-html="Car 6">
+												<img class="img-fluid img-thumbnail" src="images/ZalCars6-Gold.jpg" />
+											</a>
+										</div>
+									</div>
+								</div>
