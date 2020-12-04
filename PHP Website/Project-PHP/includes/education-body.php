741be826406cdb3a46cf226416c295f4c38fe0f7
reltaher <43248170+reltaher@users.noreply.github.com>
2020-12-04 16:58:44 -0500

    Add files via upload

--- PHP Website/Project-PHP/includes/education-body.php
+++ PHP Website/Project-PHP/includes/education-body.php
@@ -1,21 +1,51 @@
-<div id="about" class="section">
+                <div class="container">
 
-		<div class="container">
+                  <div class="text-center">
 
-        <h4 class="aboutHeading">EDUCATION</h4>
-        <hr>
-        
-        <h5><a href="http://krasiniak.pl/znani-absolwenci/">High School, 1967: Sigismond Krasinski Lyceum, Ciechanów, Poland
-        </a></h5>
-          
-          <hr>
-          <h5><a href="http://www.elka.pw.edu.pl/eng">MSc in Electronics (Control Engineering), 1973, Warsaw University of Technology, Faculty of Electronics
-          </a></h5>
-          
-          <hr>
-          <h5><a href="https://www.ee.pw.edu.pl/en/">PhD in Electrical Engineering (Computer Science & Engineering), 1978, Warsaw University of Technology, Faculty of Electrical Engineering
-          </a></h5>
+                    <h1 class="jumbotron-heading">Education</h1>
+                    <hr>
 
-				</div>
+                  </div>
 
-		</div>
+                </div>
+
+                <div class="container">
+
+                  <div class="row">
+
+                    <div class="col text-center">
+                      
+                      <h5><a href="http://krasiniak.pl/znani-absolwenci/">High School, 1967: Sigismond Krasinski Lyceum, Ciechanów, Poland
+                      </a></h5>
+
+                    </div>
+
+                  </div>
+
+                  <div class="row">
+
+                    <div class="col text-center">
+                      
+                      <hr>
+                      <h5><a href="http://www.elka.pw.edu.pl/eng">MSc in Electronics (Control Engineering), 1973, Warsaw University of Technology, Faculty of Electronics
+                      </a></h5>
+
+                    </div>
+
+                  </div>
+
+                  <div class="row">
+
+                    <div class="col text-center">
+                      
+                      <hr>
+                      <h5><a href="https://www.ee.pw.edu.pl/en/">PhD in Electrical Engineering (Computer Science & Engineering), 1978, Warsaw University of Technology, Faculty of Electrical Engineering
+                      </a></h5>
+
+                    </div>
+
+                  </div>
+
+                </div>
+
+              </div>
