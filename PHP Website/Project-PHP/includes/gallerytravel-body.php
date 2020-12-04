6a491a367d09eb48363a15d4c773773ffb5c819c
krislowg <46728217+krislowg@users.noreply.github.com>
2020-12-04 16:36:42 -0500

    Gallery Travel updated

--- PHP Website/Project-PHP/includes/gallerytravel-body.php
+++ PHP Website/Project-PHP/includes/gallerytravel-body.php
@@ -1,4 +1,4 @@
-								<div class="container">
+<div class="container">
 
 									<div class="text-center">
 
@@ -6,27 +6,60 @@
 										<hr>
 
 									</div>
+								</div>
+								<div class="album py-5 bg-light">
+									<div class="container">
+										<div class="row">
 
-									<div class="row">
-										<div id="lightgallery">
-											<a class="gallery" href="images/Poland1.jpg" data-sub-html="Poland 1">
-												<img class="img-fluid img-thumbnail" src="images/Poland1.jpg" />
-											</a>
-											<a class="gallery" href="images/Poland2.jpg">
-												<img class="img-fluid img-thumbnail" src="images/Poland2.jpg" />
-											</a>
-											<a class="gallery" href="images/Poland3.jpg">
-												<img class="img-fluid img-thumbnail" src="images/Poland3.jpg" />
-											</a>
-											<a class="gallery" href="images/Poland4.jpg">
-												<img class="img-fluid img-thumbnail" src="images/Poland4.jpg" />
-											</a>
-											<a class="gallery" href="images/Poland5.jpg">
-												<img class="img-fluid img-thumbnail" src="images/Poland5.jpg" />
-											</a>
-											<a class="gallery" href="images/Poland6.jpg">
-												<img class="img-fluid img-thumbnail" src="images/Poland6.jpg" />
-											</a>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow gallery">
+													<img class="card-img-top" src="images/travel/AmsterdamHeineken.jpg" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">Heineken Brewery in Amsterdam.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow gallery">
+													<img class="card-img-top" src="images/travel/KerouacStreet.jpg" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">Kerouac Street in San Francisco.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow gallery">
+													<img class="card-img-top" src="images/travel/Manhattan2.JPG" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">Sample of Manhattan.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow gallery">
+													<img class="card-img-top" src="images/travel/Mural-Praga2.JPG" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">Mural in Warsaw.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow gallery">
+													<img class="card-img-top" src="images/travel/SF-tram3.jpg" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">Streetcar in San Francisco.</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="card mb-4 box-shadow gallery">
+													<img class="card-img-top" src="images/travel/TrinidadColorado2.JPG" alt="Card image cap">
+													<div class="card-body">
+														<p class="card-text">Trinidad in Colorado.</p>
+													</div>
+												</div>
+											</div>																																										
 										</div>
 									</div>
 								</div>
+							</div>
