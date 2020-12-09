8aa37572aa374c81418b5adb9bfb3db3c1df60de
krislowg <46728217+krislowg@users.noreply.github.com>
2020-12-09 18:00:37 -0500

    Update theatre-body.php content and pictures added

--- PHP Website/Project-PHP/includes/theatre-body.php
+++ PHP Website/Project-PHP/includes/theatre-body.php
@@ -1,8 +1,8 @@
-								<div class="container">
+									<div class="container">
 
 									<div class="text-center">
 
-										<h1 class="jumbotron-heading">Theatre Reviews</h1>
+										<h1 class="jumbotron-heading">Theatre</h1>
 										<hr>
 
 									</div>
@@ -14,42 +14,34 @@
 									<div class="row">
 
 										<div class="col text-center">
-
-											<h5><a href="#">Theatre review one</a></h5>
 											
-											<p>by .... </p>
-											<hr>
+											<h5>Polish Theatre</h5>
+											<img src="images/theatre/theatreovation.jpg">
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi odit possimus rem doloribus quas tenetur quia praesentium officia assumenda, similique dolore sapiente saepe dolorum deleniti illo incidunt rerum aspernatur quo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, modi eius praesentium tempore nihil eos accusamus, ipsum fugiat non ducimus deleniti error tenetur iste porro quam, voluptate animi repellendus et.</p>
+											</br>
+											<h6>Directors</h6>
+											<p>Krzysztof Warlikowski, Grzegorz Jarzyna, Jacek Strama, and Emma Blacklay-Piech</p>
+
+											<h6>Actors</h6>
+											<p>Andrzej Seweryn, Tadeusz Łomnicki, Anna Chodakowska and Anna Krauze</p>					
+											<hr>	
 
 										</div>
-
 									</div>
 
 									<div class="row">
 
 										<div class="col text-center">
 											
-											<h5><a href="#">Theatre review two</a></h5>
-											
-											<p>by .... </p>
-											<hr>
+											<h5>Plays</h5>
+											<img src="images/theatre/characterplay.jpg">
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi odit possimus rem doloribus quas tenetur quia praesentium officia assumenda, similique dolore sapiente saepe dolorum deleniti illo incidunt rerum aspernatur quo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, modi eius praesentium tempore nihil eos accusamus, ipsum fugiat non ducimus deleniti error tenetur iste porro quam, voluptate animi repellendus et.</p>
+											</br>
+											<h6>Famous plays</h6>
+											<p style="font-style: italic;">Balladyna, Dwoje biednych Rumunów mówiących po polsku, Dziady, Iridion, and Wierny robot</p>														
 
+											<img src="images/theatre/emptytheatre.jpg">
 										</div>
-
-									</div>
-
-									<div class="row">
-
-										<div class="col text-center">
-											
-											<h5><a href="#">Theatre review three</a></h5>
-											
-											<p>by .... </p>
-											<hr>
-
-										</div>
-
-									</div>
+									</div>								
 
 								</div>
-
-							</div>
