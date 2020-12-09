d79a061ab4cc3897e446384dcd0e5bdf982603ce
krislowg <46728217+krislowg@users.noreply.github.com>
2020-12-09 18:58:58 -0500

    Update publications-body.php content added

--- PHP Website/Project-PHP/includes/publications-body.php
+++ PHP Website/Project-PHP/includes/publications-body.php
@@ -10,19 +10,57 @@
 								</div>
 
 								<div class="container">
+									<div class="row">
+
+										<div class="col text-center">
+
+											<h3 >2019</h3>
+										  </br>
+											<h5>IoT Safety: State of the Art</h5>
+											<p>Janusz Zalewski</p>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe voluptatum quidem exercitationem maxime nobis sunt. Maiores illo accusantium eos? Quas obcaecati perferendis saepe temporibus incidunt fugit quae maiores modi porro!</p>
+											<hr>
+
+										</div>
+									</div>
 
 									<div class="row">
 
 										<div class="col text-center">
-											
-											<h5><a href="#">Publication one</a></h5>
-											<p>Text </p>
-											<hr>
-											
-										</div>
 
+											<h3 >2018</h3>
+											</br>
+											<h5>Safety and Security Analysis of Control Chains in SCADA Using the NFR Approach</h5>
+											<p>Nary Subramanian, Janusz Zalewski</p>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe voluptatum quidem exercitationem maxime nobis sunt. Maiores illo accusantium eos? Quas obcaecati perferendis saepe temporibus incidunt fugit quae maiores modi porro!</p>
+											</br>
+
+											<h5>Introducing a Cybersecurity Mindset into Software Engineering Undergraduate Courses</h5>
+											<p>Ingrid A. Buckley, Peter J. Clarke, Janusz Zalewski</p>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe voluptatum quidem exercitationem maxime nobis sunt. Maiores illo accusantium eos? Quas obcaecati perferendis saepe temporibus incidunt fugit quae maiores modi porro!</p>
+											<hr>
+
+										</div>
 									</div>
 									
-								</div>
+									<div class="row">
 
-							</div>
+										<div class="col text-center">
+
+											<h3 >2017</h3>
+										  </br>
+											<h5>Evolution in the Education of Software Engineers: Online Course on Cyberphysical Systems with Remote Access to Robotic Devices</h5>
+											<p>Janusz Zalewski, Fernando Gonzalez</p>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe voluptatum quidem exercitationem maxime nobis sunt. Maiores illo accusantium eos? Quas obcaecati perferendis saepe temporibus incidunt fugit quae maiores modi porro!</p>
+											</br>
+
+											<h5>From Embedded Systems to Cyberphysical Systems to the Internet of Things: Consequences for STEM Education</h5>
+											<p>Janusz Zalewski, Dahai Guo, Robert Kenny and Xiaoxue Wang</p>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe voluptatum quidem exercitationem maxime nobis sunt. Maiores illo accusantium eos? Quas obcaecati perferendis saepe temporibus incidunt fugit quae maiores modi porro!</p>
+											<hr>
+
+										</div>
+									</div>
+									
+									
+								</div>
