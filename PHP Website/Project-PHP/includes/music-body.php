ab2ea04fb1b5e1ec2e737041eed243e545970ec1
krislowg <46728217+krislowg@users.noreply.github.com>
2020-12-09 17:56:59 -0500

    Update music-body.php content and pictures added

--- PHP Website/Project-PHP/includes/music-body.php
+++ PHP Website/Project-PHP/includes/music-body.php
@@ -2,7 +2,7 @@
 
 									<div class="text-center">
 
-										<h1 class="jumbotron-heading">Music Reviews</h1>
+										<h1 class="jumbotron-heading">Music</h1>
 										<hr>
 
 									</div>
@@ -14,29 +14,36 @@
 									<div class="row">
 
 										<div class="col text-center">
+											
+											<h5>The Beatles</h5>
+											<img src="images/music/thebeatleswall.jpg">
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi odit possimus rem doloribus quas tenetur quia praesentium officia assumenda, similique dolore sapiente saepe dolorum deleniti illo incidunt rerum aspernatur quo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, modi eius praesentium tempore nihil eos accusamus, ipsum fugiat non ducimus deleniti error tenetur iste porro quam, voluptate animi repellendus et.</p>
+											</br>
+											<h6>Top Albums</h6>
+											<p>'Magical Mystery Tour' (1967), 'Let It Be' (1970), 'A Hard Day's Night' (1964), 'Help!' (1965), and 'Abbey Road' (1969)</p>
 
-											<h5><a href="#">Black Sabbath</a></h5>
-
-											<p>Playlist</p>
-											<hr>
+											<h6>Top Songs</h6>
+											<p> 'Hello, Goodbye', 'Yes It Is', 'Long, Long, Long', 'All I've Got to Do', and 'Within You Without You'</p>					<hr>	
 
 										</div>
-
 									</div>
 
 									<div class="row">
 
 										<div class="col text-center">
+											
+											<h5>Black Sabbath</h5>
+											<img src="images/music/concert.jpg">
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi odit possimus rem doloribus quas tenetur quia praesentium officia assumenda, similique dolore sapiente saepe dolorum deleniti illo incidunt rerum aspernatur quo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, modi eius praesentium tempore nihil eos accusamus, ipsum fugiat non ducimus deleniti error tenetur iste porro quam, voluptate animi repellendus et.</p>
+											</br>
+											<h6>Top Albums</h6>
+											<p>'Paranoid' (1970), 'Black Sabbath' (1970), 'Black Sabbath Vol. 4' (1972), 'Master of Reality' (1971), and 'Sabbath Bloody Sabbath' (1973).</p>
 
-											<h5><a href="#">The Beatles</a></h5>
+											<h6>Top Songs</h6>
+											<p>“Black Sabbath”, “Iron Man”, “Heaven And Hell”, “War Pigs”, and “Paranoid”</p>					
 
-											<p>Playlist</p>
-											<hr>
-
+											<img src="images/music/rockrecord.jpg">
 										</div>
-
-									</div>
+									</div>								
 
 								</div>
-
-							</div>
