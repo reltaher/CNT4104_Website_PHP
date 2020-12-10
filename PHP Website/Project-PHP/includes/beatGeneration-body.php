6a44eba9e6296c32e887cea192f06d50521f5061
reltaher <43248170+reltaher@users.noreply.github.com>
2020-12-09 23:36:06 -0500

    Add files via upload

--- PHP Website/Project-PHP/includes/beatGeneration-body.php
+++ PHP Website/Project-PHP/includes/beatGeneration-body.php
@@ -1,52 +1,33 @@
-								<div class="container">
+<div class="section">
 
-									<div class="text-center">
+	<div class="container">
+		<div class="text-center">
 
-										<h1 class="jumbotron-heading">The Beat Generation</h1>
-										<hr>
+			<h1 class="jumbotron-heading">The Beat Generation</h1>
+			<hr>
 
-									</div>
+		</div>
+	</div>
 
-								</div>
+	<div class="container">
+		<div class="row">
 
-									<div class="container">
+			<div class="col text-center">
+				
+				<h5><a href="http://www.inter-zone.org/adwinans3.html">Photo at The Beat Museum, San Francisco</a></h5>
+				<p>A.D. Winans, 2011</p>
+				<hr>
 
-										<div class="row">
+				<h5><a href="http://writingpolishdiaspora.blogspot.com/2010/07/ferlinghetti-in-polish.html">Caffè Trieste, San Francisco</a></h5>
+				<p>Lawrence Ferlinghetti, 2010</p>
+				<hr>
 
-											<div class="col text-center">
+				<h5><a href="https://en.wikipedia.org/wiki/Jack_Hirschman">At the City Lights Bookstore, San Francisco</a></h5>
+				<p>Jack Hirschman, 2007</p>
+				<hr>
+			</div>
+		</div>	
+		
+	</div>
 
-												<h5><a href="http://www.inter-zone.org/adwinans3.html">Photo at The Beat Museum, San Francisco</a></h5>
-												<p>A.D. Winans, 2011</p>
-												<hr>
-
-											</div>
-
-										</div>
-
-										<div class="row">
-
-											<div class="col text-center">
-
-												<h5><a href="http://writingpolishdiaspora.blogspot.com/2010/07/ferlinghetti-in-polish.html">Caffè Trieste, San Francisco</a></h5>
-												<p>Lawrence Ferlinghetti, 2010</p>
-												<hr>
-
-											</div>
-
-										</div>
-
-										<div class="row">
-
-											<div class="col text-center">
-
-												<h5><a href="https://en.wikipedia.org/wiki/Jack_Hirschman">At the City Lights Bookstore, San Francisco</a></h5>
-												<p>Jack Hirschman, 2007</p>
-												<hr>
-
-											</div>
-
-										</div>
-
-									</div>
-
-								</div>
+</div>
--- PHP Website/Project-PHP/includes/writers-body.php
+++ PHP Website/Project-PHP/includes/writers-body.php
@@ -1,49 +1,31 @@
-								<div class="container">
+<div class="section">
 
-									<div class="text-center">
+	<div class="container">
+		<div class="text-center">
 
-										<h1 class="jumbotron-heading">Polish-American Writers</h1>
-										<hr>
+			<h1 class="jumbotron-heading">Polish American Writers</h1>
+			<hr>
 
-									</div>
+		</div>
+	</div>
 
-									<div class="container">
+	<div class="container">
+		<div class="row">
+			<div class="col text-center">
+				<h5><a href="http://writingpolishdiaspora.blogspot.com/2013/02/douglas-blazek-at-moes-in-berkeley.html">Reading in Berkeley and Moe’s Bookstore</a></h5>
+				<p>Douglas Blazek, 2013</p>
+				<hr>
+				
+				<h5><a href="http://www.stephenpoleskie.com/works.htm">Photo at the Rauschenberg Painting</a></h5>
+				<p>Steve Poleskie, 2011</p>
+				<hr>
+				
+				<h5><a href="https://www.jstor.org/stable/20148780?seq=1">Magical Polonism</a></h5>	
+				<p>Article in Polish American Studies, 2007</p>
+				<hr>
+			</div>
+		</div>	
 
-										<div class="row">
+	</div>
 
-											<div class="col text-center">
-
-												<h5><a href="http://writingpolishdiaspora.blogspot.com/2013/02/douglas-blazek-at-moes-in-berkeley.html">Reading in Berkeley and Moe’s Bookstore</a></h5>
-												<p>Douglas Blazek, 2013</p>
-												<hr>
-
-											</div>
-										</div>
-
-										<div class="row">
-											<div class="col text-center">
-
-												<h5><a href="http://www.stephenpoleskie.com/works.htm">Photo at the Rauschenberg Painting</a></h5>
-												<p>Steve Poleskie, 2011</p>
-												<hr>
-
-											</div>
-										</div>
-
-										<div class="row">
-											<div class="col text-center">
-
-
-												<h5><a href="https://www.jstor.org/stable/20148780?seq=1">Magical Polonism</a></h5>	
-												<p>Article in Polish American Studies, 2007</p>
-												<hr>
-
-											</div>
-
-										</div>
-
-									</div>
-
-								</div>
-
-							</div>
+</div>
