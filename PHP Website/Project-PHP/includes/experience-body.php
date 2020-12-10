69e97eef198292152cdf0680cfe9a2b1bcbd4ee1
krislowg <46728217+krislowg@users.noreply.github.com>
2020-12-09 19:01:44 -0500

    Update experience-body.php content added

--- PHP Website/Project-PHP/includes/experience-body.php
+++ PHP Website/Project-PHP/includes/experience-body.php
@@ -10,21 +10,52 @@
 								</div>
 
 								<div class="container">
+									<div class="row">
+
+										<div class="col text-center">
+
+											<h3 >2019</h3>
+										  </br>
+											<h5>Experience one</h5>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe voluptatum quidem exercitationem maxime nobis sunt. Maiores illo accusantium eos? Quas obcaecati perferendis saepe temporibus incidunt fugit quae maiores modi porro!</p>
+											<hr>
+
+										</div>
+									</div>
 
 									<div class="row">
 
 										<div class="col text-center">
 
-											
-											<h5><a href="#">Experience one</a></h5>
-											<p>Text </p>
+											<h3 >2018</h3>
+											</br>
+											<h5>Experience one</h5>											
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe voluptatum quidem exercitationem maxime nobis sunt. Maiores illo accusantium eos? Quas obcaecati perferendis saepe temporibus incidunt fugit quae maiores modi porro!</p>
+											</br>
+
+											<h5>Experience two</h5>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe voluptatum quidem exercitationem maxime nobis sunt. Maiores illo accusantium eos? Quas obcaecati perferendis saepe temporibus incidunt fugit quae maiores modi porro!</p>
 											<hr>
 
 										</div>
+									</div>
+									
+									<div class="row">
 
+										<div class="col text-center">
+
+											<h3 >2017</h3>
+										  </br>
+											<h5>Experience one</h5>											
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe voluptatum quidem exercitationem maxime nobis sunt. Maiores illo accusantium eos? Quas obcaecati perferendis saepe temporibus incidunt fugit quae maiores modi porro!</p>
+											</br>
+
+											<h5>Experience two</h5>										
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe voluptatum quidem exercitationem maxime nobis sunt. Maiores illo accusantium eos? Quas obcaecati perferendis saepe temporibus incidunt fugit quae maiores modi porro!</p>
+											<hr>
+
+										</div>
 									</div>
 									
 									
 								</div>
-
-							</div>
