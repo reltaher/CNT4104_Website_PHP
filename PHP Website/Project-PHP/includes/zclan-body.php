be3f3b7fac1d6079aba4e330a35e8c024a2762b7
reltaher <43248170+reltaher@users.noreply.github.com>
2020-12-09 21:41:24 -0500

    Add files via upload

---
+++ PHP Website/Project-PHP/includes/zclan-body.php
@@ -0,0 +1,36 @@
+<div class="container">
+
+	<div class="text-center">
+
+		<h1 class="jumbotron-heading">The Z Clan</h1>
+		<hr>
+
+	</div>
+
+</div>
+
+<div class="container">
+
+	<div class="row">
+
+		<div class="col text-center">
+
+			<h5><a href="https://en.wikipedia.org/wiki/Krzysztof_Zalewski">Krzysztof Zalewski - Singer</h5>
+			<hr>
+
+		</div>
+
+	</div>
+
+	<div class="row">
+
+		<div class="col text-center">
+
+			<h5><a href="https://lcamtuf.coredump.cx/">Michał Zalewski - Security Engineer</h5>
+			<hr>
+
+		</div>
+
+	</div>
+
+</div>
