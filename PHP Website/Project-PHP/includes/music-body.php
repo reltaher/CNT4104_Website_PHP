49249bf6addcb53bd37df81b131638b9bd271f17
reltaher <43248170+reltaher@users.noreply.github.com>
2020-12-09 23:38:52 -0500

    Add files via upload

--- PHP Website/Project-PHP/includes/music-body.php
+++ PHP Website/Project-PHP/includes/music-body.php
@@ -1,49 +1,53 @@
-								<div class="container">
+<div class="section">
 
-									<div class="text-center">
+	<div class="container">
 
-										<h1 class="jumbotron-heading">Music</h1>
-										<hr>
+		<div class="text-center">
 
-									</div>
+			<h1 class="jumbotron-heading">Music</h1>
+			<hr>
 
-								</div>
+		</div>
 
-								<div class="container">
+	</div>
 
-									<div class="row">
+	<div class="container">
 
-										<div class="col text-center">
-											
-											<h5>The Beatles</h5>
-											<img src="images/music/thebeatleswall.jpg">
-											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi odit possimus rem doloribus quas tenetur quia praesentium officia assumenda, similique dolore sapiente saepe dolorum deleniti illo incidunt rerum aspernatur quo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, modi eius praesentium tempore nihil eos accusamus, ipsum fugiat non ducimus deleniti error tenetur iste porro quam, voluptate animi repellendus et.</p>
-											</br>
-											<h6>Top Albums</h6>
-											<p>'Magical Mystery Tour' (1967), 'Let It Be' (1970), 'A Hard Day's Night' (1964), 'Help!' (1965), and 'Abbey Road' (1969)</p>
+		<div class="row">
 
-											<h6>Top Songs</h6>
-											<p> 'Hello, Goodbye', 'Yes It Is', 'Long, Long, Long', 'All I've Got to Do', and 'Within You Without You'</p>					<hr>	
+			<div class="col text-center">
+				
+				<h5>The Beatles</h5>
+				<img src="images/music/thebeatleswall.jpg">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi odit possimus rem doloribus quas tenetur quia praesentium officia assumenda, similique dolore sapiente saepe dolorum deleniti illo incidunt rerum aspernatur quo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, modi eius praesentium tempore nihil eos accusamus, ipsum fugiat non ducimus deleniti error tenetur iste porro quam, voluptate animi repellendus et.</p>
+			</br>
+			<h6>Top Albums</h6>
+			<p>'Magical Mystery Tour' (1967), 'Let It Be' (1970), 'A Hard Day's Night' (1964), 'Help!' (1965), and 'Abbey Road' (1969)</p>
 
-										</div>
-									</div>
+			<h6>Top Songs</h6>
+			<p> 'Hello, Goodbye', 'Yes It Is', 'Long, Long, Long', 'All I've Got to Do', and 'Within You Without You'</p>					<hr>	
 
-									<div class="row">
+		</div>
+	</div>
 
-										<div class="col text-center">
-											
-											<h5>Black Sabbath</h5>
-											<img src="images/music/concert.jpg">
-											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi odit possimus rem doloribus quas tenetur quia praesentium officia assumenda, similique dolore sapiente saepe dolorum deleniti illo incidunt rerum aspernatur quo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, modi eius praesentium tempore nihil eos accusamus, ipsum fugiat non ducimus deleniti error tenetur iste porro quam, voluptate animi repellendus et.</p>
-											</br>
-											<h6>Top Albums</h6>
-											<p>'Paranoid' (1970), 'Black Sabbath' (1970), 'Black Sabbath Vol. 4' (1972), 'Master of Reality' (1971), and 'Sabbath Bloody Sabbath' (1973).</p>
+	<div class="row">
 
-											<h6>Top Songs</h6>
-											<p>“Black Sabbath”, “Iron Man”, “Heaven And Hell”, “War Pigs”, and “Paranoid”</p>					
+		<div class="col text-center">
+			
+			<h5>Black Sabbath</h5>
+			<img src="images/music/concert.jpg">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi odit possimus rem doloribus quas tenetur quia praesentium officia assumenda, similique dolore sapiente saepe dolorum deleniti illo incidunt rerum aspernatur quo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, modi eius praesentium tempore nihil eos accusamus, ipsum fugiat non ducimus deleniti error tenetur iste porro quam, voluptate animi repellendus et.</p>
+		</br>
+		<h6>Top Albums</h6>
+		<p>'Paranoid' (1970), 'Black Sabbath' (1970), 'Black Sabbath Vol. 4' (1972), 'Master of Reality' (1971), and 'Sabbath Bloody Sabbath' (1973).</p>
 
-											<img src="images/music/rockrecord.jpg">
-										</div>
-									</div>								
+		<h6>Top Songs</h6>
+		<p>“Black Sabbath”, “Iron Man”, “Heaven And Hell”, “War Pigs”, and “Paranoid”</p>					
 
-								</div>
+		<img src="images/music/rockrecord.jpg">
+	</div>
+</div>								
+
+</div>
+
+</div>
--- PHP Website/Project-PHP/includes/theatre-body.php
+++ PHP Website/Project-PHP/includes/theatre-body.php
@@ -1,47 +1,51 @@
-									<div class="container">
+<div class="section">
 
-									<div class="text-center">
+	<div class="container">
 
-										<h1 class="jumbotron-heading">Theatre</h1>
-										<hr>
+		<div class="text-center">
 
-									</div>
+			<h1 class="jumbotron-heading">Theatre</h1>
+			<hr>
 
-								</div>
+		</div>
 
-								<div class="container">
+	</div>
 
-									<div class="row">
+	<div class="container">
 
-										<div class="col text-center">
-											
-											<h5>Polish Theatre</h5>
-											<img src="images/theatre/theatreovation.jpg">
-											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi odit possimus rem doloribus quas tenetur quia praesentium officia assumenda, similique dolore sapiente saepe dolorum deleniti illo incidunt rerum aspernatur quo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, modi eius praesentium tempore nihil eos accusamus, ipsum fugiat non ducimus deleniti error tenetur iste porro quam, voluptate animi repellendus et.</p>
-											</br>
-											<h6>Directors</h6>
-											<p>Krzysztof Warlikowski, Grzegorz Jarzyna, Jacek Strama, and Emma Blacklay-Piech</p>
+		<div class="row">
 
-											<h6>Actors</h6>
-											<p>Andrzej Seweryn, Tadeusz Łomnicki, Anna Chodakowska and Anna Krauze</p>					
-											<hr>	
+			<div class="col text-center">
+				
+				<h5>Polish Theatre</h5>
+				<img src="images/theatre/theatreovation.jpg">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi odit possimus rem doloribus quas tenetur quia praesentium officia assumenda, similique dolore sapiente saepe dolorum deleniti illo incidunt rerum aspernatur quo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, modi eius praesentium tempore nihil eos accusamus, ipsum fugiat non ducimus deleniti error tenetur iste porro quam, voluptate animi repellendus et.</p>
+			</br>
+			<h6>Directors</h6>
+			<p>Krzysztof Warlikowski, Grzegorz Jarzyna, Jacek Strama, and Emma Blacklay-Piech</p>
 
-										</div>
-									</div>
+			<h6>Actors</h6>
+			<p>Andrzej Seweryn, Tadeusz Łomnicki, Anna Chodakowska and Anna Krauze</p>					
+			<hr>	
 
-									<div class="row">
+		</div>
+	</div>
 
-										<div class="col text-center">
-											
-											<h5>Plays</h5>
-											<img src="images/theatre/characterplay.jpg">
-											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi odit possimus rem doloribus quas tenetur quia praesentium officia assumenda, similique dolore sapiente saepe dolorum deleniti illo incidunt rerum aspernatur quo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, modi eius praesentium tempore nihil eos accusamus, ipsum fugiat non ducimus deleniti error tenetur iste porro quam, voluptate animi repellendus et.</p>
-											</br>
-											<h6>Famous plays</h6>
-											<p style="font-style: italic;">Balladyna, Dwoje biednych Rumunów mówiących po polsku, Dziady, Iridion, and Wierny robot</p>														
+	<div class="row">
 
-											<img src="images/theatre/emptytheatre.jpg">
-										</div>
-									</div>								
+		<div class="col text-center">
+			
+			<h5>Plays</h5>
+			<img src="images/theatre/characterplay.jpg">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi odit possimus rem doloribus quas tenetur quia praesentium officia assumenda, similique dolore sapiente saepe dolorum deleniti illo incidunt rerum aspernatur quo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, modi eius praesentium tempore nihil eos accusamus, ipsum fugiat non ducimus deleniti error tenetur iste porro quam, voluptate animi repellendus et.</p>
+		</br>
+		<h6>Famous plays</h6>
+		<p style="font-style: italic;">Balladyna, Dwoje biednych Rumunów mówiących po polsku, Dziady, Iridion, and Wierny robot</p>														
 
-								</div>
+		<img src="images/theatre/emptytheatre.jpg">
+	</div>
+</div>								
+
+</div>
+
+</div>
