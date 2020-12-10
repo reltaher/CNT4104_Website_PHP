6b2faf93c93700c1d002b6dc1452c0ee16805bcc
reltaher <43248170+reltaher@users.noreply.github.com>
2020-12-09 21:41:07 -0500

    Add files via upload

---
+++ PHP Website/Project-PHP/zclan.php
@@ -0,0 +1,9 @@
+<?php
+	define("TITLE", "Janusz Zalewski | The Z Clan");
+
+	include('includes/header.php');
+?>
+
+<?php include('includes/zclan-body.php'); ?>
+
+<?php include('includes/footer.php'); ?>
